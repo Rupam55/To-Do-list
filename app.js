2c8365ffca4771e373fcc9a488ffee43b483c709
Rupam55 <rupamkarmakar55@gmail.com>
2020-06-07 00:12:24 +0530

    with search

--- app.js
+++ app.js
@@ -1,49 +1,27 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const User = require("./models/users.js");
+const Item = require("./models/item.js");
+const List = require("./models/list.js");
+
 const _ = require("lodash");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
-const itemsSchema = {
-  name: String
-};
-
-const Item = mongoose.model("Item", itemsSchema);
-
-const item1 = new Item({
-  name: "Welcome to your todolist!"
-});
-
-const defaultItems = [item1];
-
-const listSchema = {
-  user: String,
-  name: String,
-  items: [itemsSchema]
-};
-
-const List = mongoose.model("List", listSchema);
-
-const userSchema = {
-  email: String,
-  password: String,
-};
-
-const User = mongoose.model("user", userSchema);
-
 // custom variables
 
-var usern="";
+var usern = "";
+var date_search= "";
+var category_search="";
+var went="false";
 
 // get route
 
@@ -55,81 +33,112 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 app.get("/lg", function (req, res) {
-  usern="";
+  usern = "";
   console.log("logout");
   res.redirect("/");
 });
 
-
-
-app.get("/", function(req, res) {
-
-  Item.find({}, function(err, foundItems){
-
-    if (foundItems.length === 0) {
-      Item.insertMany(defaultItems, function(err){
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Successfully savevd default items to DB.");
-        }
-      });
-      res.redirect("/");
-    } else {
-      console.log(usern);
-      res.render("list", {userName: usern ,listTitle: "Today", newListItems: foundItems});
-    }
-  });
-
+app.get("/", function (req, res) {
+  res.render("landing", { userName: usern });
 });
 
-app.get("/:customListName", function(req, res){
-  const customListName = _.capitalize(req.params.customListName);
+app.get("/category", function (req, res) {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = today.getFullYear();
 
-  List.findOne({user:usern , name: customListName}, function(err, foundList){
-    if (!err){
-      if (!foundList){
-        //Create a new list
-        const list = new List({
-          user: usern,
-          name: customListName,
-          items: defaultItems
-        });
-        list.save();
-        res.redirect("/" + customListName);
-      } else {
-        //Show an existing list
-          res.render("list", {userName: foundList.user , listTitle: foundList.name, newListItems: foundList.items,});
-      }
+  today = yyyy + "-" + mm + "-" + dd;
+  if (usern === "") {
+    res.redirect("/login");
+  } else {
+    if(went==="false"){
+      date_search="";
+      category_search="";
     }
-  });
-
-
-
+    Item.find({ user: usern }).sort({ date: 1 }).then((posts) => {
+        res.render("list", {
+          userName: usern,
+          listTitle: "All Tasks",
+          newListItems: posts,
+          today: today,
+          date_s:date_search, 
+          category_s: category_search
+        });
+        went="false";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 });
 
 // post route
 
-app.post("/customlist", function(req, res){
+app.post("/category", function (req, res) {
+  const category = req.body.newList;
+  const task = req.body.task;
+  const date = req.body.duedate;
 
-  const List = req.body.newList;
-  res.redirect("/" + List);
-  
+  const item = new Item({
+    task: task,
+    date: date,
+    user: usern,
+    category: category,
+  });
+  if (usern === "") {
+    res.redirect("/login");
+  } else {
+    item.save();
+    res.redirect("/category");
+  }
+});
+
+app.post("/search", function (req, res) {
+  const category = req.body.newList;
+  const date = req.body.date;
+
+  if (usern === "") {
+    res.redirect("/login");
+  } else {
+    category_search=category;
+    date_search=date;
+    went="true"; 
+    res.redirect("/category")
+  }
 });
 
 app.post("/register", function (req, res) {
+  const email = req.body.username;
+  const password = req.body.password;
+
   const newUser = new User({
-    email: req.body.username,
-    password: req.body.password,
+    email: email,
+    password: password,
   });
 
   newUser.save(function (err) {
     if (err) {
       console.log(err);
     } else {
-      usern=req.body.username;
+      usern = req.body.username;
+
+      /*  var today = new Date();
+      var dd = String(today.getDate()).padStart(2, "0");
+      var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+      var yyyy = today.getFullYear();
+
+      today = yyyy + "-" + mm + "-" + dd;
+      const defaultItems = new Item({
+        task: "Add New Task",
+        date: today,
+        user: usern,
+        category: "Personal",
+      });
+      defaultItems.save();
+ */
       console.log(usern);
-      res.redirect("/");
+      res.redirect("/category");
     }
   });
 });
@@ -138,70 +147,62 @@ app.post("/login", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email:username}, function (err, foundUser) {
+  User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
       console.log(err);
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          usern=foundUser.email;
-          res.redirect("/")
-        }
+      res.redirect("/register");
+    } else if (foundUser) {
+      if (foundUser.password === password) {
+        usern = foundUser.email;
+        res.redirect("/category");
       } else {
-        console.log("go to the registration page fool");
+        res.redirect("login");
       }
+    } else {
+      res.redirect("/register");
     }
   });
 });
 
-app.post("/", function(req, res){
-
+/* app.post("/", function (req, res) {
   const itemName = req.body.newItem;
   const listName = req.body.list;
 
   const item = new Item({
-    name: itemName
+    name: itemName,
   });
 
-  if (listName === "Today"){
-    if(usern===""){
-      item.save();
-      res.redirect("/");
-    }else{
-      res.redirect("/Today "+usern)
+  if (listName === "Today") {
+    if (usern === "") {
+      res.redirect("/login");
     }
   } else {
-    List.findOne({user: usern, name: listName}, function(err, foundList){
-
+    List.findOne({ user: usern, name: listName }, function (err, foundList) {
       foundList.items.push(item);
       foundList.save();
       res.redirect("/" + listName);
     });
   }
-});
+}); */
 
-app.post("/delete", function(req, res){
+app.post("/delete", function (req, res) {
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
 
-  if (listName === "Today") {
-    Item.findByIdAndRemove(checkedItemId, function(err){
+  if (listName === "All Tasks") {
+    Item.findByIdAndRemove(checkedItemId, function (err) {
       if (!err) {
         console.log("Successfully deleted checked item.");
-        res.redirect("/");
+        res.redirect("/category");
       }
     });
   } else {
-    List.findOneAndUpdate({user:usern , name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
-      if (!err){
-        res.redirect("/" + listName);
-      }
-    });
+    res.redirect("/category");
   }
- 
-
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log("server started at 3000 port");
 });
